3b7e75597698f5657e0abb863e43c4610f21c7e6
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2022-01-31 20:44:37 +0330

    feat: Added Storage and Temp folders

--- DockerVolumeConceptProject/Controllers/HomeController.cs
+++ DockerVolumeConceptProject/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using DockerVolumeConceptProject.Models;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,26 +14,22 @@ namespace DockerVolumeConceptProject.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IWebHostEnvironment env,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _env = env;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
