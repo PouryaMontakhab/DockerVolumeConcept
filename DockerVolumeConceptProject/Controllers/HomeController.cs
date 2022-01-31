bc25e9d4a91506687b8186ecc8422234b71c1ad8
pourya.montakhabii@yahoo.com <pourya.montakhabii@yahoo.com>
2022-01-31 21:11:01 +0330

    feat: Added Save action in home controller

--- DockerVolumeConceptProject/Controllers/HomeController.cs
+++ DockerVolumeConceptProject/Controllers/HomeController.cs
@@ -7,17 +7,19 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DockerVolumeConceptProject.Controllers
 {
+    //source 
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IWebHostEnvironment _env;
         private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger,IWebHostEnvironment env,
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment env,
             IConfiguration configuration)
         {
             _logger = logger;
@@ -29,7 +31,39 @@ namespace DockerVolumeConceptProject.Controllers
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public IActionResult Save(string title, string body)
+        {
+            //if you want to create automatically Storage folder
+            if (!Directory.Exists($"{_env.ContentRootPath}\\Storage\\"))
+                Directory.CreateDirectory($"{_env.ContentRootPath}\\Storage\\");
+            //if you want to create automatically Temp folder
+            if (!Directory.Exists($"{_env.ContentRootPath}\\Temp\\"))
+                Directory.CreateDirectory($"{_env.ContentRootPath}\\Temp\\");
+
+            //temporary data
+            var tempPath = $"{_env.ContentRootPath}\\Temp\\{title}.txt";
+            using (FileStream fs = System.IO.File.Create(tempPath))
+            {
+                byte[] content = new UTF8Encoding(true).GetBytes($"{body}");
+
+                fs.Write(content, 0, content.Length);
+            }
+
+            //persistant data
+            var storagePath = $"{_env.ContentRootPath}\\Storage\\{title}.txt";
+            if (IsExistFile(storagePath))
+            {
+                System.IO.File.Delete(tempPath);
+                return Content("This file already exist");
+            }
+
+            System.IO.File.Move(tempPath, storagePath);
+            return RedirectToAction("Index");
+        }
+        public bool IsExistFile(string path)
+            => System.IO.File.Exists(path);
 
     }
 }
